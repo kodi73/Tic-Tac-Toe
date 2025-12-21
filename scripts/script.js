e95fa823866b6d8fcd0f2f80d572c2f481854656
kodi73 <aditya.kumar.73@outlook.com>
2025-12-21 17:19:00 +0530

    Added EvaluateBoard function

--- scripts/script.js
+++ scripts/script.js
@@ -35,7 +35,8 @@ const GameController = (function () {
     let gameOver = false;
 
     const player1 = Player("Player 1", "X");
-    const player2 = Player("Player 2", "O");
+    const player2 = Player("Computer", "O");
+    const isSinglePlayer = true;
 
     let activePlayer = player1;
 
@@ -112,8 +113,39 @@ const GameController = (function () {
         reset,
         checkWinner,
         checkTie,
+        isSinglePlayer,
+        winningCombinations,
     };
 })();
+
+/*
+    Evaluate Board function
+*/
+const evaluateBoard = (board) => {
+  const winPatterns = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6],
+  ];
+
+  for (let pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (
+      board[a] &&
+      board[a] === board[b] &&
+      board[a] === board[c]
+    ) {
+      return board[a];
+    }
+  }
+
+  if (board.every(cell => cell !== "")) {
+    return "tie";
+  }
+
+  return null;
+};
+
 
 const DisplayController = (function () {
     const statusText = document.getElementById("status");
